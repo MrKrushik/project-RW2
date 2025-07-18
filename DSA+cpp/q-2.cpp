9343c7a0774723f6069da672489be96f303c669c
Krushik Rakholiya <itzmekrushik90999@gmail.com>
2025-07-18 10:29:13 +0530

    Update q-2.cpp

--- DSA+cpp/q-2.cpp
+++ DSA+cpp/q-2.cpp
@@ -4,37 +4,38 @@ using namespace std;
 int main()
 {
     int n;
-    cout << endl << "enter any number: ";
-    cin >> n;
-
-    int a;
+    int rem;
+    int rev = 0;
     int i = 0;
-    int sum;
-    int digit;
+    int x = 0;
+    int y;
 
-    a = n;
+    cout << endl << "Enter the Number :- ";
+    cin >> n;
+    y = n;
 
-    while (a > 0)
+    rem = n;
+    while (rem != 0)
+    {
+        rev = rev * 10 + (rem % 10);
+        rem /= 10;
+    }
+    while (rev != 0)
     {
         i++;
-        a /= 10;
+        int digit = rev % 10;
+        int num = 1;
+
+        for (int j = 1; j <= i; j++)
+                num *= digit;
+        
+        x += num;
+        rev /= 10;
     }
-
-    sum = 0;
-    a = n;
-
-    while (a > 0)
-    {
-        digit = a % 10;
-        sum += (digit, i);
-        a /= 10;
-        i--;
-    }
-
-    if (sum == n)
-        cout << n << " is a Disarium Number" << endl;
+    if (x == y)
+        cout << y << " is a Disarium Number";
+    
     else
-        cout << n << " is a not Disarium Number" << endl;
-
-        cout << endl;
+        cout << y << " is not a Disarium Number";
+    
 }
